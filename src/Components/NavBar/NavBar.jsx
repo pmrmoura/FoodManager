9d1c97da77f4e101fc9da55ef72a1407860c7d56
Pedro da Matta <pmrm@cin.ufpe.br>
2020-11-30 12:00:08 -0300

    Adds logout feature

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
 import './NavBar.css';
 
 
 export default function NavBar({ gerente }) {
+    const [cookie, setCookie] = useCookies()
+
+    function handleLogOut() {
+        cookie.setCookie('token', null)
+    }
     return (
         <div className="navbar-wrapper">
             <h2> FOOD MANAGER </h2>
             <div className="right-content-navbar">
                 <h3>BEM VINDO, {gerente}</h3>
-                <p>Sair</p>
+                <p onClick={handleLogOut}>Sair</p>
             </div>
         </div>
     );
