a19f00c5cd72b5b00ede45767e9e54ebb2e2d313
apfdamascena <apfd@cin.ufpe.br>
2020-11-30 03:13:16 -0300

    conectando...

--- src/Components/Pages/MainPage/MainPage.js
+++ src/Components/Pages/MainPage/MainPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '../../../services/api';
 
 import BoxInformation from '../../BoxInformation/BoxInformation';
 import Footer from '../../Footer/Footer';
@@ -8,6 +9,27 @@ import Updates from '../../Updates/Updates';
 import './MainPage.css';
 
 function MainPage() {
+  const [cookies, setCookies] = useState({}); // aqui já tem o email e a senha 
+  const [products, setProducts ] = useState([]);
+
+  useEffect(() => {
+    loadInfo();
+    const {id} = cookies;
+    const domain = `api/restaurant/${id}`;
+    API.get(domain).then((response) => {
+       const { products } = response;
+       setProducts(products);
+    }).catch((error) => {
+      console.log(error);
+    })
+  })
+
+  function loadInfo(){
+    let currentCookies = document.cookie;
+    let object_cookies = JSON.parse(currentCookies);
+    setCookies(object_cookies);
+  }
+
   return (
     <div>
       <NavBar gerente="sofia" />
