67c6605c30f1569ecb51925f5e324f1ce2762db3
Pedro da Matta <pmrm@cin.ufpe.br>
2020-11-30 14:15:12 -0300

    Adds feedback

--- src/Components/BoxUpdates/BoxUpdates.jsx
+++ src/Components/BoxUpdates/BoxUpdates.jsx
@@ -3,29 +3,17 @@ import './BoxUpdates.css'
 
 
 function BoxUpdates({ title, content, children }) {
-    const [clicked, setClicked] = useState(false);
-
-    function handleBoxClick() {
-        setClicked(!clicked);
-    }
-
-    if (clicked) {
-        return (
-            <div className="box-update-wrapper" onClick={handleBoxClick} id="change-color">
-                <div>
-                    <h3 id="change-color">{title}</h3>
-                    <p id="change-color" >{content}</p>
-                </div>
-
-                {children}
-            </div>
-        );
-    }
+    console.log(content, 'content')
     return (
-        <div className="box-update-wrapper" onClick={handleBoxClick}>
+        <div className="box-update-wrapper">
             <div className="box-update-info">
                 <h3>{title}</h3>
-                <p >{content}</p>
+                {content !== "NaN%" && (
+                    <p>{content}</p>
+                )}
+                {content === "NaN%" && (
+                    <p>0%</p>
+                )}
             </div>
 
             {children}
